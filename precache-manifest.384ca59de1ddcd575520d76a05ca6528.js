0557c6dc3c1af16caa68e326d62c24b5db5124a0
mjt <thapa.manish16@gmail.com>
2019-07-07 09:35:39 +0545

    Updates

---
+++ precache-manifest.384ca59de1ddcd575520d76a05ca6528.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6214fb00756ebe196d79e00a61ceea50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87a2bf9432c15019327f",
+    "url": "/static/css/main.614b9a97.chunk.css"
+  },
+  {
+    "revision": "a968690d5cdd82cddb20",
+    "url": "/static/js/2.4a792116.chunk.js"
+  },
+  {
+    "revision": "87a2bf9432c15019327f",
+    "url": "/static/js/main.c4b90cd9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
